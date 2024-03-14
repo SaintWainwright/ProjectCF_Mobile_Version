0b882ec0d0901b045bc88523f47c57a1a8f68e9c
Wayneson <88691836+SaintWainwright@users.noreply.github.com>
2024-03-14 23:48:36 +0800

    Added services for json file handling.

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,10 +1,13 @@
-﻿namespace ProjectCF_Mobile_Version
+﻿using ProjectCF_Mobile_Version.View;
+
+namespace ProjectCF_Mobile_Version
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(MainPage), typeof(MainPage));
         }
     }
 }
---
+++ Services/Employee_Services.cs
@@ -0,0 +1,28 @@
+﻿using ProjectCF_Mobile_Version.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ProjectCF_Mobile_Version.Services
+{
+    class Employee_Services
+    {
+        string filePath = Path.Combine(FileSystem.Current.AppDataDirectory, "Employee.txt");
+        public ObservableCollection<Employee> GetEmployees()
+        {
+            if (!File.Exists(filePath))
+            {
+                return new ObservableCollection<Employee>();
+            }
+
+            string FileUsers = File.ReadAllText(filePath);
+            var EmployeeList = JsonSerializer.Deserialize<ObservableCollection<Employee>>(FileUsers);
+
+            return EmployeeList;
+        }
+    }
+}
