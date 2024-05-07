bf2e0c4432968476ebb29fd723b4bb72a2fb9b58
HansVincent <vinvenjamin.gravinez.21@usjr.edu.ph>
2024-05-07 23:17:56 +0800

    Added: MessageTagText and Color converters and utilized the converter for the View Message XAML

---
+++ Converter/MessageTagTextConverter.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace ProjectCF_Mobile_Version.Converter
+{
+    public class MessageTagTextConverter : IValueConverter
+    {
+        public string tagText;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int tag = (int)value;
+            if (tag == 0)
+            {
+                return tagText = "Unread";
+            }
+            else if (tag == 1)
+            {
+                return tagText = "Read";
+            }
+            else if (tag == 2)
+            {
+                return tagText = "Pending";
+            }
+            else if (tag == 3)
+            {
+                return tagText = "Approved";
+            }
+            else if (tag == 4)
+            {
+                return tagText = "Rejected";
+            }
+            else
+            {
+                return tagText = "Error";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
